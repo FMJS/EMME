211c3712f7ea6769c357ab37d020eb9cb03eae85
Cristian Mattarei <mattarei@stanford.edu>
2017-02-23 09:30:01 -0800

    Fixed script for v8 installation

--- ext_tools/get_v8.sh
+++ ext_tools/get_v8.sh
@@ -1,6 +1,8 @@
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=`pwd`/depot_tools:"$PATH"
 mkdir v8
 cd v8
 fetch v8
 cd v8
 ./tools/dev/v8gen.py x64.release
-ninja -C out.gn/x64.release -j 20
+ninja -C out.gn/x64.release -j 4
