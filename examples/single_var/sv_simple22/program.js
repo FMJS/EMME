a09e53ec1607e4b42b9acc5b22c064d7e14a28ec
Cristian Mattarei <mattarei@stanford.edu>
2017-03-02 18:03:28 -0800

    Extended with additional examples

---
+++ examples/single_var/sv_simple22/program.js
@@ -0,0 +1,24 @@
+if (this.Worker) {
+(function execution() {
+var t1 =
+`onmessage = function(data) {
+var x = new Int16Array(data.x_sab); id2_R_t1 = Atomics.load(x, 0); print("id2_R_t1: "+id2_R_t1);
+var x = new Int8Array(data.x_sab); Atomics.store(x, 0, 1);
+var x = new Int8Array(data.x_sab); Atomics.store(x, 1, 1);
+};`;
+var t2 =
+`onmessage = function(data) {
+var x = new Int16Array(data.x_sab); id5_R_t2 = Atomics.load(x, 0); print("id5_R_t2: "+id5_R_t2);
+var x = new Int8Array(data.x_sab); Atomics.store(x, 0, 2);
+var x = new Int8Array(data.x_sab); Atomics.store(x, 1, 2);
+};`;
+var data = {
+x_sab : new SharedArrayBuffer(8),
+}
+ 
+var wt1 = new Worker(t1);
+var wt2 = new Worker(t2);
+wt1.postMessage(data, [data.x_sab]);
+wt2.postMessage(data, [data.x_sab]);
+})();
+}
