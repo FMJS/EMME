a854ebba715a02f939b186a8804c7200c79da63d
Cristian Mattarei <mattarei@stanford.edu>
2017-03-17 15:07:05 -0700

    enabled v8 compilation

--- ext_tools/get_v8.sh
+++ ext_tools/get_v8.sh
@@ -5,7 +5,7 @@ cd v8
 fetch v8
 cd v8
 ./tools/dev/v8gen.py x64.release
-ninja -C out.gn/x64.release -j 4
+make -j4 x64.release
 cd ../../
 echo "`pwd`/v8/v8/out.gn/x64.release/d8 --test --random-seed=\$RANDOM --use-strict --nohard-abort --nodead-code-elimination --nofold-constants `pwd`/v8/v8/test/test262/data/harness/sta.js `pwd`/v8/v8/test/test262/data/harness/assert.js `pwd`/v8/v8/test/test262/harness-adapt.js `pwd`/v8/v8/test/test262/harness-agent.js --harmony-sharedarraybuffer \$1" > run_v8.sh
 echo "for file in `find \$1 -name \"*.js\"`; do dir=\$(dirname \"\$\{file\}\") python litmus.py -c \"bash `pwd`/run_d8.sh\" -i \$file -s -n \$2 -j 1; done" > run_all_v8.sh
