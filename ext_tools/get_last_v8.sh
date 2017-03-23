3d606f2a6db4857b84609fc749a4c7e31dedc571
Cristian Mattarei <mattarei@stanford.edu>
2017-03-23 13:00:03 -0700

    removed useless file

--- ext_tools/get_last_v8.sh
+++
@@ -1,8 +0,0 @@
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-export PATH=`pwd`/depot_tools:"$PATH"
-mkdir last_v8
-cd last_v8
-fetch v8
-cd v8
-./tools/dev/v8gen.py x64.release
-ninja -C out.gn/x64.release -j 4
