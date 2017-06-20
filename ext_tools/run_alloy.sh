9f70086eaadad744061365ee34ccfdf46313aba6
Cristian Mattarei <mattarei@stanford.edu>
2017-06-20 14:32:45 -0700

    Minor changes

--- ext_tools/run_alloy.sh
+++ ext_tools/run_alloy.sh
@@ -1,2 +1,5 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd `dirname $0` > /dev/null
+DIR=`pwd -P`
+popd > /dev/null
+echo $DIR
 java -Djava.library.path=$DIR/amd64-linux/ -jar $DIR/Alloy_Interface.jar
